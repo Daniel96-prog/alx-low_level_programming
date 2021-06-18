8fc13ac2be0d7c8186d7fcdd45b93f97428d06fb
Daniel96-prog <84845882+Daniel96-prog@users.noreply.github.com>
2021-06-18 08:41:02 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
 *Description: Very simple snippet
 *Return: 0
 */
-int main()
+int main(void)
 {
 char c;
 int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of an int: %d byte(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(li));
-printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-printf("Size of a float: %d byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\r\n", sizeof(c));
+printf("Size of an int: %d byte(s)\r\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\r\n", sizeof(li));
+printf("Size of a long long int: %d byte(s)\r\n", sizeof(lli));
+printf("Size of a float: %d byte(s)\r\n", sizeof(f));
 return (0);
 }
