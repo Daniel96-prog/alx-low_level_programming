096120c544ea7b1441006f482791161b7db1def7
Daniel96-prog <84845882+Daniel96-prog@users.noreply.github.com>
2021-06-18 09:33:46 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,2 @@
 #include<stdio.h>
-int main(void){printf("%s\n", "with proper grammar, but the outcome is a piece of art,");return (0);}
+int main(void){printf("%s\n", "with proper grammar, but the outcome is a piece of art,"); return (0); }
