4f8a829120aca895e9b3f5e8273c6779b4903e49
Daniel96-prog <optleinad7@gmail.com>
2021-06-18 09:34:49 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - print to standard error
+ * Description: write function
+ * Return: return 0
+ */
+
+int main(void)
+{
+for(int x = '0'; x <='9' ; x++)
+{
+putchar(x);
+}
+return (0);
+}
