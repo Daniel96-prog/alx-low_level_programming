0a51c61e6d8e9f20dbd7fdeead73575e6afd40e2
Daniel96-prog <84845882+Daniel96-prog@users.noreply.github.com>
2021-06-18 20:11:42 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
  *Description: Prints hexadecimal digits using putchar
  *Return: 0
 */
+
 int main(void)
 {
-	int i;
-
-	for (i = 48; i <= 102; i++)
-	{
-		putchar(i);
-		if (i == 57)
-			i += 39;
-	}
-	putchar('\n');
-	return (0);
+int i;
+for (i = 48; i <= 102; i++)
+{
+putchar(i);
+if (i == 57)
+i += 39;
+}
+putchar('\n');
+return (0);
 }
