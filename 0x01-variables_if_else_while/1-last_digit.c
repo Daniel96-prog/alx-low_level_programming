818f74d77f5e35ff15c222ae95919dde46c9815b
Daniel96-prog <84845882+Daniel96-prog@users.noreply.github.com>
2021-06-18 18:59:40 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lDig);
 }
 else if (lDig < 6)
 {
-printf("Last digit of %d is %d and is less than 6\n", n, lDig);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lDig);
 }
 return (0);
 }
