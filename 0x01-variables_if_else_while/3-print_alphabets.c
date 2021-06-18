355d0dea721e29501e22ca2389e685c1747a3883
Daniel96-prog <optleinad7@gmail.com>
2021-06-18 09:18:31 -0700

    Capital included

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+  
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - print to standard error
+ * Description: write function
+ * Return: return 0
+ */
+
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(x);
+}
+
+putchar('\n');
+return (0);
+}
