f169e1dd5429cc7fafade7f6a9234af803b7c25c
Daniel96-prog <84845882+Daniel96-prog@users.noreply.github.com>
2021-06-22 18:51:39 +0300

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+/**
+*print_alphabet - abc
+*Description: prints alphabes in lower case followed by a new line
+*return: void
+*/
+int main(void)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
