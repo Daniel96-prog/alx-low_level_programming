586e82b149033d18a482f7509046924d4c2adc2b
Daniel96-prog <optleinad7@gmail.com>
2021-06-28 12:47:28 -0700

    Day 5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,21 @@
 #include <string.h>
 /**
 *_puts - puts the string
+*@str: - input string
 *Return: void
 */
 void _puts(char *str)
 {
-for(int i = 0; i < strlen(str); i++)
+int i;
+for (i = 0; i < strlen(str); i++)
 {
-putchar(str[i]);
+_putchar(str[i]);
 }
-putchar('\n');
-for(int i = strlen(str); i >= 0 ; i--)
+_putchar('\n');
+for (i = strlen(str); i >= 0 ; i--)
 {
-putchar(str[i]);
+_putchar(str[i]);
 }
-putchar('\n');
+_putchar('\n');
 return (0);
 }
