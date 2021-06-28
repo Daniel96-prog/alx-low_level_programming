bdf8848ca6764708ec0cba45090dabc424472a9e
Daniel96-prog <optleinad7@gmail.com>
2021-06-28 06:37:39 -0700

    Swaps values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+*swap_int - swaps int values
+*
+*@a: int a
+*@b: value of b
+*@c: changer value
+*Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+  int c;
+  c = *a;
+  *a = *b;
+  *b = c;
+}
