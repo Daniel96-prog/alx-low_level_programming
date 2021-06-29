7559091d9beaf6f5ff07fc9e3b6375ed9483fdef
Daniel96-prog <84845882+Daniel96-prog@users.noreply.github.com>
2021-06-29 22:49:51 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,29 @@
-#include <string.h>
+  
+#include "holberton.h"
 /**
-*rev_string - reverses a string
-*
-*Return: void
-*@s: input string
-*/
+ *rev_string - reverse the string.
+ *@s: pointer to string.
+ *
+ *Return: void.
+ */
 void rev_string(char *s)
 {
-int len, current_char, i, j;
-len = strlen(s);
-j = len - 1;
-for (i = 0; i < j; i++)
-{
-current_char = name[i];
-name[i] = name[j];
-name[j] = current_char;
-j--;
-}
+	int i, j, k, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
 }
