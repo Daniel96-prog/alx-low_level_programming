70d013ff282fcf66ceb0815a5d3a8305af4f9fba
Daniel96-prog <optleinad7@gmail.com>
2021-07-05 13:28:15 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *_memset: sets memory
+ * @s: pointer to the string
+ * @b: char to be added
+ * @n: int
+ * Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+/* add x throughout s[n] */
+unsigned char *s1;
+const unsigned char c = b;
+for (s1 = s; n > 0; ++s1, --n)
+s1 = c;
+return (s);
+}
