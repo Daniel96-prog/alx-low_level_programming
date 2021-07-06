42f56d1138659f37fe2895470b81a33308051e2b
Daniel96-prog <optleinad7@gmail.com>
2021-07-06 13:22:51 -0700

    holberton header file

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,3 +7,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
