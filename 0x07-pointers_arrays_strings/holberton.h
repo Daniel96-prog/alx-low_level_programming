8a5abae7600337dc2e8f8b8874e527c61265395d
Daniel96-prog <optleinad7@gmail.com>
2021-07-06 13:04:57 -0700

    hey

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -4,3 +4,6 @@ char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
+int _putchar(char c);
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
