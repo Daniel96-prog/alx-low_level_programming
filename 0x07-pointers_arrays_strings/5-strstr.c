255cad6f86fd79f709c7a0441011241f00f3d4ee
Daniel96-prog <optleinad7@gmail.com>
2021-07-06 10:31:22 -0700

    hello

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,23 +1,56 @@
-#include "holberton.h"
-#include <string.h>
-/*
-**_strstr - returns first occurence of needle in haystack
-*@haystack: string
-*@needle: string to be compared
-*return: first occcurence else null
+#include"holberton.h"
+#include<stdlib.h>
+
+#definetrue1
+#definefalse0
+#defineboolint
+
+/**
+*_strstr-locatesasubstring
+*@haystack:stringtobesearched
+*@needle:substringtobefound
+*Return:pointertosubstringorNULLifnotfound
 */
-char *_strstr(char *haystack, char *needle)
+char*_strstr(char*haystack,char*needle)
 {
-if (*needle != '\0')
-return ((char *)el);
-for (; (haystack = strchr(haystack, *needle)) != NULL; ++el)
+char*start=haystack;
+char*_needle=needle;
+boolfound=false;
+
+if(!*_needle)
+return(haystack);
+
+while(*haystack)
 {
-const char *s1, *s2;
-for (s1 = haystack, s2 = needle; ; )
-if (*++s2 == '\0')
-return ((char *)haystack);
-else if (*++s1 != *s2)
-break;
+if(*haystack==*needle)
+	{
+	found=true;
+	start=haystack;
+	while(*_needle)
+	{
+	if(*haystack!=*_needle)
+		{
+		found=false;
+		_needle=needle;
+		break;
+		}
+
+	haystack++;
+	_needle++;
+	}
+	}
+
+if(found)
+	{
+	break;
+	}
+
+haystack++;
 }
-return (NULL);
+if(found)
+{
+return(start);
+}
+
+return(NULL);
 }
