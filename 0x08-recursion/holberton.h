67ec468b149b595ddd61b972383e18dda2134d11
Daniel96-prog <optleinad7@gmail.com>
2021-07-08 11:50:25 -0700

    )X)* recursion

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,2 +1,21 @@
 void _puts_recursion(char *s);
 int _putchar(char c);
+/* 1-print_rev_recursion.c */
+void _print_rev_recursion(char *s);
+
+/* 2-strlen_recursion.c */
+int _strlen_recursion(char *s);
+
+/* 3-factorial.c */
+int factorial(int n);
+
+/* 4-pow_recursion.c */
+int _pow_recursion(int x, int y);
+
+/* 5-sqrt_recursion.c */
+int _sqrt_recursion(int n);
+
+/* 6-is_prime_number.c */
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
