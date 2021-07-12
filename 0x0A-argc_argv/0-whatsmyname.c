25f6aeca080ac5fc7764005bb2fe4320c4fa02f0
Daniel96-prog <optleinad7@gmail.com>
2021-07-12 04:26:22 -0700

    prints name of file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
 *main - prints the name of the file
+*@argv: array of string arguments
 *return: 0
 */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 printf("%s", argv[0]);
 return (0);
