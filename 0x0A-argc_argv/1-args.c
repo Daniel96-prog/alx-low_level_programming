f9fa80986b8708d7f4cb82e1d2de2a594ec4b969
Daniel96-prog <optleinad7@gmail.com>
2021-07-12 06:23:15 -0700

    shiuld work

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 *main - prints number of arguments
 *@argc: number of arguments
 *@argv: array of arguments
-*Return - returns 0
+*Return: returns 0
 */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
