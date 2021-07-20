ad476b303adabb28b8baa25ceedcf3e3cc154730
Daniel96-prog <optleinad7@gmail.com>
2021-07-20 09:53:16 -0700

    Project 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+  
+#ifndef STRTYP
+#define STRTYP
+
+/**
+  *struct dog - structure of a dog.
+  *@name: name of dog.
+  *@age: age of dog.
+  *@owner: owner of dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*STRTYP*/
